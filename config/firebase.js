40d9e7b702f22cf8a238eced731a3c37ea806b8f
Fahim Morshed <51060061+fahshed@users.noreply.github.com>
2025-04-13 18:48:17 -0400

    Add files via upload

---
+++ config/firebase.js
@@ -0,0 +1,15 @@
+const admin = require("firebase-admin");
+
+let serviceAccount;
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  serviceAccount = require("./serviceAccountKey.json");
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const db = admin.firestore();
+module.exports = db;
